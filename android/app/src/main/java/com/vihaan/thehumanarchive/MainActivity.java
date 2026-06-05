f34985a1a68778c1421f1c4001f745a7e6a7c52e
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-06-05 16:08:05 +0530

    Capacitor setup

---
+++ android/app/src/main/java/com/vihaan/thehumanarchive/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vihaan.thehumanarchive;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vihaan.thehumanarchive',
+  appName: 'the-human-archive',
+  webDir: 'public'
+};
+
+export default config;
